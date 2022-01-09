5fe04cf6a4e9535bace291e034e979adfb22b1af
durgesh <durgesh.acharya2006@gmail.com>
2022-01-09 21:45:11 +0530

    get request made for consignor

--- lib/model/consignor.dart
+++ lib/model/consignor.dart
@@ -1,3 +1,69 @@
-class Consignor{
+// To parse this JSON data, do
+//
+//     final consignor = consignorFromJson(jsonString);
 
+import 'dart:convert';
+
+Consignor consignorFromJson(String str) => Consignor.fromJson(json.decode(str));
+
+String consignorToJson(Consignor data) => json.encode(data.toJson());
+
+class Consignor {
+    Consignor({
+        required this.consignorid,
+        required this.consignorcoo,
+        required this.consignorassociate,
+         required this.consignorname,
+       required this.consignoraddress,
+       required this.consignorpincode,
+       required this.consignormob,
+       required this.consignoremail,
+       required this.consignorgst,
+       required this.consignoractive,
+        required this.consignorpassword,
+      required this.consignordatetime,
+    });
+
+    int consignorid;
+    int consignorcoo;
+    int consignorassociate;
+    String consignorname;
+    String consignoraddress;
+    int consignorpincode;
+    String consignormob;
+    String consignoremail;
+    String consignorgst;
+    int consignoractive;
+    String consignorpassword;
+    String consignordatetime;
+
+    factory Consignor.fromJson(Map<String, dynamic> json) => Consignor(
+        consignorid: json["consignorid"],
+        consignorcoo: json["consignorcoo"],
+        consignorassociate: json["consignorassociate"],
+        consignorname: json["consignorname"],
+        consignoraddress: json["consignoraddress"],
+        consignorpincode: json["consignorpincode"],
+        consignormob: json["consignormob"],
+        consignoremail: json["consignoremail"],
+        consignorgst: json["consignorgst"],
+        consignoractive: json["consignoractive"],
+        consignorpassword: json["consignorpassword"],
+        consignordatetime: json["consignordatetime"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "consignorid": consignorid,
+        "consignorcoo": consignorcoo,
+        "consignorassociate": consignorassociate,
+        "consignorname": consignorname,
+        "consignoraddress": consignoraddress,
+        "consignorpincode": consignorpincode,
+        "consignormob": consignormob,
+        "consignoremail": consignoremail,
+        "consignorgst": consignorgst,
+        "consignoractive": consignoractive,
+        "consignorpassword": consignorpassword,
+        "consignordatetime": consignordatetime,
+    };
 }
--- lib/screens/consignorscreen.dart
+++ lib/screens/consignorscreen.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shipsmart/model/consignor.dart';
 import 'package:shipsmart/screens/addconsignor.dart';
+import 'package:http/http.dart' as http;
 
 class ConsignorScreen extends StatefulWidget {
   const ConsignorScreen({ Key? key }) : super(key: key);
@@ -9,6 +13,19 @@ class ConsignorScreen extends StatefulWidget {
 }
 
 class _ConsignorScreenState extends State<ConsignorScreen> {
+    int associate = 1;
+    
+    Future<Consignor?> getConsignor()async{
+      
+      var response = await http.get(Uri.parse("http://shipsmart-env.eba-4nc5kenf.us-east-2.elasticbeanstalk.com/consignor/byassociate/${associate}"));
+      var jsonresponse = jsonDecode(response.body);
+
+      if(response.statusCode == 200){
+        return Consignor.fromJson((jsonresponse));
+      }
+     
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
